0b7a8ff462ad34a51f8ed7eacdad2ca21829547e
Paul Ryley <paul@geminilabs.io>
2017-07-06 11:23:10 -0400

    rewrite gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,152 +1,130 @@
-var plugin = 'gridable',
-	source_SCSS = { admin: './admin/scss/**/*.scss', public: './public/scss/**/*.scss'},
-	dest_CSS = { admin:'./admin/css/', public: './public/css/'},
+var args            = require('yargs').argv;
+var autoprefixer    = require('gulp-autoprefixer');
+var bump            = require('gulp-bump');
+var checktextdomain = require('gulp-checktextdomain');
+var concat          = require('gulp-concat');
+var cssnano         = require('gulp-cssnano');
+var exec            = require('gulp-exec');
+var gulp            = require('gulp-help')(require('gulp'));
+var gulpif          = require('gulp-if');
+var mergeStream     = require('merge-stream');
+var plumber         = require('gulp-plumber');
+var potomo          = require('gulp-potomo');
+var pseudo          = require('gulp-pseudo-i18n');
+var rename          = require('gulp-rename');
+var runSequence     = require('run-sequence');
+var sass            = require('gulp-sass');
+var sort            = require('gulp-sort');
+var wpPot           = require('gulp-wp-pot');
+var yaml            = require('yamljs');
 
-	gulp 		= require('gulp'),
-	sass 		= require('gulp-ruby-sass'),
-	prefix 		= require('gulp-autoprefixer'),
-	exec 		= require('gulp-exec'),
-	replace 	= require('gulp-replace'),
-	minify 		= require('gulp-minify-css'),
-	concat 		= require('gulp-concat'),
-	notify 		= require('gulp-notify'),
-	beautify 	= require('gulp-beautify'),
-	csscomb 	= require('gulp-csscomb'),
-	cmq 		= require('gulp-combine-media-queries'),
-	chmod 		= require('gulp-chmod'),
-	fs          = require('fs'),
-	rtlcss 		= require('rtlcss'),
-	postcss 	= require('gulp-postcss'),
-	del         = require('del'),
-	rename 		= require('gulp-rename');
+var config = yaml.load('config.yml');
 
-require('es6-promise').polyfill();
-
-
-var options = {
-	silent: true,
-	continueOnError: true // default: false
-};
-
-// styles related
-gulp.task('styles-admin', function () {
-	return gulp.src(source_SCSS.admin)
-		.pipe(sass({'sourcemap': false, style: 'compact'}))
-			.on('error', function (e) {
-				console.log(e.message);
-			})
-		.pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7"))
-		.pipe(gulp.dest(dest_CSS.admin));
+/* CSS Task
+ -------------------------------------------------- */
+gulp.task('styles', 'Compile all stylesheets.', function() {
+  return gulp.src( config.watch.scss, { base: '.' })
+  .pipe(plumber({ errorHandler: function(error) {
+      console.log(error.message);
+      this.emit('end');
+  }}))
+  .pipe(sass({
+    outputStyle: 'compact',
+    sourcemap: false,
+  }))
+  .pipe(autoprefixer('last 1 version', '> 1%', 'ie 8', 'ie 7'))
+  .pipe(gulpif(args.production, cssnano({
+    discardComments: { removeAll: true }
+  })))
+  .pipe(rename(function(path) {
+    path.dirname = path.dirname.replace('scss', 'css');
+  }))
+  .pipe(gulp.dest('.'))
+},{
+	options: { 'production': 'Minify the stylesheets.' }
 });
 
-gulp.task('styles-public', function () {
-	return gulp.src(source_SCSS.public)
-		.pipe(sass({'sourcemap': false, style: 'compact'}))
-		.on('error', function (e) {
-			console.log(e.message);
-		})
-		.pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7"))
-		.pipe(gulp.dest(dest_CSS.public));
+/* Watch Task
+ -------------------------------------------------- */
+gulp.task('watch', 'Watch all stylesheets for changes.', function() {
+  return gulp.watch( config.watch.scss, ['styles']);
+},{
+	options: { 'production': 'Minify the stylesheets.' }
 });
 
-gulp.task('styles', ['styles-admin', 'styles-public'], function () {
-	// ok
+/* Archive Task
+ -------------------------------------------------- */
+gulp.task('archive', 'Generate the plugin archive zip file.', function() {
+  return gulp.src('./').pipe(exec('git archive -o ../gridable.zip HEAD'));
 });
 
-gulp.task('watch-admin', function () {
-	return gulp.watch(source_SCSS.admin, ['styles-admin']);
+/* Default Task
+ -------------------------------------------------- */
+gulp.task('default', false, function() {
+  gulp.start('styles', 'languages');
 });
 
-gulp.task('watch-public', function () {
-	return gulp.watch(source_SCSS.public, ['styles-public']);
+/* Version Task
+ -------------------------------------------------- */
+gulp.task('bump', 'Incrementally increase the plugin version.', function() {
+  ['patch', 'minor', 'major'].some(function(arg) {
+    if(!args[arg])return;
+    for(var key in config.bump) {
+      gulp.src(config.bump[key]).pipe(bump({
+        type: arg,
+        key: key,
+      })).pipe(gulp.dest('.'));
+    }
+    return true;
+  });
+},{
+	options: {
+		'patch': 'e.g. v0.0.1',
+		'minor': 'e.g. v0.1.0',
+		'major': 'e.g. v1.0.0',
+	}
 });
 
-/**
- * Create a zip archive out of the cleaned folder and delete the folder
- */
-gulp.task( 'zip', ['build'], function() {
-	return gulp.src( './' )
-		.pipe( exec( 'cd ./../; rm -rf gridable.zip; cd ./build/; zip -r -X ./../gridable.zip ./gridable; cd ./../; rm -rf build' ) );
-
-} );
-
-/**
- * Copy theme folder outside in a build folder, recreate styles before that
- */
-gulp.task( 'copy-folder', function() {
-	return gulp.src( './' )
-		.pipe( exec( 'rm -Rf ./../build; mkdir -p ./../build/gridable; cp -Rf ./* ./../build/gridable/' ) );
-} );
-
-/**
- * Clean the folder of unneeded files and folders
- */
-gulp.task( 'build', ['copy-folder'], function() {
-
-	// files that should not be present in build zip
-	files_to_remove = [
-		'**/codekit-config.json',
-		'node_modules',
-		'tests',
-		'.travis.yml',
-		'circle.yml',
-		'phpunit.xml.dist',
-		'.sass-cache',
-		'config.rb',
-		'gulpfile.js',
-		'package.json',
-		'pxg.json',
-		'build',
-		'.idea',
-		'**/*.css.map',
-		'**/.git*',
-		'*.sublime-project',
-		'.DS_Store',
-		'**/.DS_Store',
-		'__MACOSX',
-		'**/__MACOSX',
-		'+development.rb',
-		'+production.rb',
-		'README.md',
-		'.labels'
-	];
-
-	files_to_remove.forEach( function( e, k ) {
-		files_to_remove[k] = '../build/gridable/' + e;
-	} );
-
-	del.sync(files_to_remove, {force: true});
-} );
-
-// usually there is a default task  for lazy people who just wanna type gulp
-gulp.task('default', ['styles'], function () {
-	// silence
+/* Language Tasks
+ -------------------------------------------------- */
+gulp.task('languages', 'Generate the plugin language files.', function() {
+  return runSequence('po', 'mo')
 });
-
-/**
- * Short commands help
- */
-
-gulp.task('help', function () {
-
-	var $help = '\nCommands available : \n \n' +
-		'=== General Commands === \n' +
-		'start              (default)Compiles all styles and scripts and makes the theme ready to start \n' +
-		'zip                Generate the zip archive \n' +
-		'build              Generate the build directory with the cleaned theme \n' +
-		'help               Print all commands \n' +
-		'=== Style === \n' +
-		'styles             Compiles styles in production mode\n' +
-		'styles-dev         Compiles styles in development mode \n' +
-		'styles-admin       Compiles admin styles \n' +
-		'=== Scripts === \n' +
-		'scripts            Concatenate all js scripts \n' +
-		'scripts-dev        Concatenate all js scripts \n' +
-		'=== Watchers === \n' +
-		'watch              Watches all js and scss files \n' +
-		'styles-watch       Watch only styles\n' +
-		'scripts-watch      Watch scripts only \n';
-
-	console.log($help);
-
+gulp.task('po', false, function() {
+  return gulp.src(config.language.src)
+  .pipe(checktextdomain({
+    text_domain: config.language.domain,
+    keywords: [
+      '__:1,2d',
+      '_e:1,2d',
+      '_x:1,2c,3d',
+      'esc_html__:1,2d',
+      'esc_html_e:1,2d',
+      'esc_html_x:1,2c,3d',
+      'esc_attr__:1,2d',
+      'esc_attr_e:1,2d',
+      'esc_attr_x:1,2c,3d',
+      '_ex:1,2c,3d',
+      '_n:1,2,4d',
+      '_nx:1,2,4c,5d',
+      '_n_noop:1,2,3d',
+      '_nx_noop:1,2,3c,4d',
+    ],
+  }))
+  .pipe(sort())
+  .pipe(wpPot({
+    domain: config.language.domain,
+    lastTranslator: config.language.translator,
+    team: config.language.team,
+  }))
+  .pipe(pseudo({
+    charMap: {},
+  }))
+  .pipe(rename(config.language.domain + '-en_US.po'))
+  .pipe(gulp.dest(config.language.dest));
+});
+gulp.task('mo', false, function() {
+  return gulp.src(config.language.dest + '*.po')
+  .pipe(potomo())
+  .pipe(gulp.dest(config.language.dest));
 });
